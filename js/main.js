c170f30e7bcea68b6752a228b04ca828c17008f0
rezalmp7 <rezalwahyu10@gmail.com>
2021-07-30 03:01:01 +0700

    dev

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+
+    $('#gallery').simplegallery({
+        galltime: 400,
+        gallcontent: '.content',
+        gallthumbnail: '.thumbnail',
+        gallthumb: '.thumb'
+    });
+
+});
+$(document).ready(function () {
+    $('#datatables').DataTable();
+});
